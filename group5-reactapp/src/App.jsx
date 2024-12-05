3043279c5cecda8fe2026201ae0d489240d19b1f
Veerpal <veerpal17011999@gmail.com>
2024-12-05 00:24:49 -0500

    modify App.jsx for testing purpose

--- group5-reactapp/src/App.jsx
+++ group5-reactapp/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React , { useState } from 'react'; // Import React and useState
+import Calculate from './Calculate'; // Import the Calculate component
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0); // Initialize state variable count
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Calculate /> {/* Include the Calculate component */}
+      <h1>Hello!, This is group5 project</h1>
+      <h2>This is first React Vite project for learning Git workflow</h2>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <p>Current Count: {count}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
